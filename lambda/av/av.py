41407d9919562592651fade3fe7634174b783bea
Victor de Alencar Britto <valencarbritto@gmail.com>
2025-11-17 17:38:58 -0300

    change

--- lambda/av/av.py
+++ lambda/av/av.py
@@ -2,7 +2,10 @@ DOMAIN_NAME = "1clickparish.com"
 DEFAULT_EXCEPTION_MESSAGE = 'Your Google Account was rejected.'
 
 def lambda_handler(event, context):
-    if 'hd' not in event['request']['userAttributes']: raise Exception(DEFAULT_EXCEPTION_MESSAGE)
-    if event['request']['userAttributes']['hd'] == DOMAIN_NAME: event['response']['autoConfirmUser'] = True
-    else: raise Exception(DEFAULT_EXCEPTION_MESSAGE)
+    trigger = event.get('triggerSource')
+    user_attributes = event.get('request', {}).get('userAttributes', {}) or {}
+    hosted_domain = user_attributes.get('custom::hd')
+    if trigger != 'PreSignUp_ExternalProvider': return event
+    if not hosted_domain or hosted_domain.lower() != DOMAIN_NAME.lower(): raise Exception(DEFAULT_EXCEPTION_MESSAGE)
+    event['response']['autoVerifyEmail'] = True
     return event
